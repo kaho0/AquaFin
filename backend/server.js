00763fd00e7ac821532a463805248d856321b8ba
kahon <kahonbintezaman@gmail.com>
2025-08-04 22:41:26 +0600

    url changed

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 // ✅ Apply CORS before defining routes
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173"], // ✅ Localhost for development
+    origin: ["https://aquafin.onrender.com"], // ✅ Localhost for development
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // ✅ Needed for authentication
